3c0a2d20199e52bbc266de47e4ea0256838a54f8
m.aantjes <65569213+MaxAantjes@users.noreply.github.com>
2020-06-08 22:54:25 -0500

    Removed data cleaning section
    Data cleaning section is removed to section 3 and rural.codes and urban.codes are stored to minimize calculations when rerunning code.

--- Module 2.R
+++ Module 2.R
@@ -1,5 +1,4 @@
-## This module manipulates the data to distinguish between rural 
-## and urban migration and origin. 
+## This module generates a list of rural and urban postal codes.  
 
 
 ## Load packages and data.Download list of postal codes and
@@ -9,7 +8,6 @@ library(pacman)
 pacman::p_load(pdftools)
 pacman::p_load(stringr)
 
-dat0 <- readRDS("clean_data")
 td = tempdir()
 temp = tempfile(tmpdir=td, fileext=".pdf")
 download.file("https://aplicaciones2.ecuadorencifras.gob.ec/SIN/descargas/cge2019.pdf",temp, mode = "wb")
@@ -56,55 +54,11 @@ rural.codes <- as.integer(unlist(str_extract_all(rural.text,
 urban.codes <- as.integer(unlist(str_extract_all(urban.text, 
                                 "[0-9][0-9][0-9][0-9][0-9][0-9]")))
 
+saveRDS(rural.codes, file = "rural_codes.csv")
+saveRDS(urban.codes, file = "urban_codes.csv")
+rm(list =ls())
 
-## Whether codes are rural or urban can be checked with the 
-## following function. This can be (and has been) used to test 
-## the validity of the above method. 
 
-rurORurb <- function(code, v1 = rural.codes, v2 = urban.codes) {
-        
-        if (is.na(code) == TRUE) {
-                
-                c <- code
-                
-        } else {
-        
-        a <- sum(grepl(code, v1))
-        b <- sum(grepl(code, v2))
-        
-        if(a>0) {
-                
-                c <- "rural"}
-        
-        if(b>0) {
-                
-                c <- "urban"} 
-        
-        if(b==0 & a ==0) {
-                
-                c <- "code not found"
-        }
-        
-        }
-        
-        return(c)
-}
-
-## Replace codes in original data set with factor variable
-## (urban and rural). 
-
-dat1 <- dat0
-
-dat1$place.of.birth <- sapply(dat0$place.of.birth, rurORurb)
-
-dat1$location.prior.address <- sapply(dat1$location.prior.address, 
-                                   rurORurb)
-
-dat1$current.address <- sapply(dat1$current.address, 
-                                      rurORurb)
-
-## Save data
-saveRDS(dat1, "clean_data_urban_rural")
 
 
 
