b5b213eee8defb683ce1277730d3b0ab2720777e
m.aantjes <65569213+MaxAantjes@users.noreply.github.com>
2020-06-08 22:53:24 -0500

    Make module 1 compatible with 0
    Removed download file section, as this is taken over by module 0. Raw data from module 0 is read into module 1 through readRDS. Data cleaning section moved to module 3.

--- Module 1.R
+++ Module 1.R
@@ -1,76 +1,73 @@
 ## This module cleans and tidies the data. 
 
+
 ## Load packages and data set. 
 ## run install.packages("pacman") if you do not have this package.
 
 library(pacman)
 pacman::p_load(dplyr)
-td = tempdir()
-temp = tempfile(tmpdir=td, fileext=".zip")
-download.file("https://www.ecuadorencifras.gob.ec/documentos/web-inec/EMPLEO/2019/Septiembre/BDD_DATOS_ABIERTOS_ENEMDU_%202019_09_CSV.zip",temp)
-unzip(temp, exdir=td, overwrite=TRUE)
-dat0 <- read.csv(file.path(td, "BDD_DATOS_ABIERTOS_ENEMDU_ 2019_09_CSV/enemdu_personas_2019_09.csv"), sep =';')
-unlink(temp)
+
+dat0 <- readRDS("raw_data")
 
 
-## Select columns of interest (location of origin, migration, higher education 
-## level and age).Select rows of interest(age from 20 to 25)
+## running list() on dat0 reveals that the data sets have different variables.
+## For this reason we will reduce each data frame to a set of variables which are available
+## for each element data frame of the list and which are relevant for our analysis. 
+## We select the following columns of interest (related to location of origin, 
+## migration, higher education level and age).Select rows of interest(age from 20 to 25)
+## Whether these variables were available and similar for each year and period can be
+## verified by checking the used "formulario" in every respective raw data folder linked 
+## in the codebook. 
 
-dat1 <- dat0 %>%
-        select(ciudad, p03, p15, p15aa, p15ab, p16a, p16b, p17a, p17b, p10a, 
-               p10b, p07, p12a, p12b) %>%
-        filter(p03 > 17 & p03 < 25) 
 
+dat1 <- lapply(dat0, select, survey.location, p03, p15, p15aa, p15ab, p16a, p16b, p17a, p17b, p10a, 
+       p10b, p07, p12a, p12b, date)
+dat2 <- rbind(dat1[[1]], dat1[[2]], dat1[[3]], dat1[[4]], dat1[[5]],
+              dat1[[6]], dat1[[7]])
 
 ## Tidy columnnames. Create factor variables in line with codebook. 
 
-names(dat1) <- c("current.address", "age", "ethnicity", 
+names(dat2) <- c("current.address", "age", "ethnicity", 
                  "born.at.current.address", "place.of.birth", "ever.moved", 
                  "time.at.current.address", "prior.address.abroad", 
                  "location.prior.address", "higher.education.level", 
                  "no.years.completed", "currently.matriculated",
-                 "obtained.degree", "field.of.degree")
+                 "obtained.degree", "field.of.degree", "date") #"current.address"
 
-dat1$higher.education.level <- factor(dat1$higher.education.level, 
+dat2$current.address <- factor(dat2$current.address, level = c(1,2),
+                               labels = c("urban", "rural"))
+
+dat2$higher.education.level <- factor(dat2$higher.education.level, 
                                       levels = 1:10, labels = 
                                          c("none", "none", "none", "none",
                                            "none", "none", "none", 
                                            "technical.school",
                                            "undergraduate", "postgraduate"))
-dat1$born.at.current.address <- factor(dat1$born.at.current.address, labels =
-                       c("yes", "no.as.national", "no.as.international"))
-dat1$obtained.degree <- factor(dat1$obtained.degree, levels = c(1,2), labels =
+
+dat2$born.at.current.address <- factor(dat2$born.at.current.address, level =
+                                        c(1,2,3), labels = c("yes", 
+                                        "no.as.national", "no.as.international"))
+
+dat2$obtained.degree <- factor(dat2$obtained.degree, levels = c(1,2), labels =
                                        c("yes", "no"))
-dat1$ever.moved <- factor(dat1$ever.moved, levels = c(1,2),
+
+dat2$ever.moved <- factor(dat2$ever.moved, levels = c(1,2),
                                     labels = c("no", "yes"))
-dat1$prior.address.abroad <- factor(dat1$prior.address.abroad, levels = 
+
+dat2$prior.address.abroad <- factor(dat2$prior.address.abroad, levels = 
                                          c(1,2), labels = c("no","yes"))
-dat1$ethnicity <- factor(dat1$ethnicity, levels = 1:8, labels = c("indigenous",
+dat2$ethnicity <- factor(dat2$ethnicity, levels = 1:8, labels = c("indigenous",
                                 "afroecuadorian", "black", "mulatto",
                                 "montubio", "mestizo", "white", "other"))
-dat1$currently.matriculated <- factor(dat1$currently.matriculated, levels =
+
+dat2$currently.matriculated <- factor(dat2$currently.matriculated, levels =
                                               c(1,2), labels = c("yes", "no"))
 
 
-## Select rows where place.of.birth with survey.location is the same as 
-## place.of.birth. Replace place.of.birth with survey.location. 
-
-dat2 <- dat1 %>%
-        filter(born.at.current.address == "yes") %>%
-        mutate(place.of.birth = current.address) %>%
-        select(-born.at.current.address)
-
-
-## Now we merge the data sets again and exclude foreign born 
-## Ecuadorians.
-
-dat3 <- dat1 %>%
-        filter(born.at.current.address == "no.as.national") %>%
-        select(-born.at.current.address) %>%
-        rbind(dat2)
-
 
 ## Save data
-saveRDS(dat3, "clean_data")
+saveRDS(dat2, "modified_data_1.csv")
+rm(list = ls())
+
         
 
