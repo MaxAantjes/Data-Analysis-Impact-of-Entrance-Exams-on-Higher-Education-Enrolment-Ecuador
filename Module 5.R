65ff6ce6f76c60c75587ec2ec83486c10bdd22b1
m.aantjes <65569213+MaxAantjes@users.noreply.github.com>
2020-06-15 00:03:24 -0500

    Removed Linear Models
    Removed Linear Models as they do not seem to fit the data well.

--- Module 5.R
+++ Module 5.R
@@ -6,41 +6,32 @@ pacman::p_load(ggplot2)
 PA.table <- readRDS("past.address.table.csv")
 PB.table <- readRDS("place.of.birth.table.csv")
 CA.table <- readRDS("current.address.table.csv")
+migration.table <- readRDS("academic.migration.csv")
 
 ## GOAL: Create plot and regression model for CURRENT ADDRESS
 ## method. 
 CA.plot <- ggplot(CA.table, aes(x=survey.date, y=ratio)) + geom_point(size = 2) + 
         labs(title = "Ratio of weighed proportions of rural/ urban population enrolled as first year undergraduate students", 
-                          subtitle = "Rural and urban identification by current address. Population 17-39 y/o.",
+                          subtitle = "Rural and urban identification by current address. Population 17-39 y/o. Period Dec 2007 to Sep 2019.",
                           x ="survey date", y ="proportion ratio")                                        
-                                           
-mod0 <- lm(ratio ~ survey.date, data= CA.table) 
-summary(mod0)
-CA.plot2 <- CA.plot + geom_smooth(method = "lm", 
-                                             col = "blue")
-
+print(CA.plot)
 
 ## GOAL: Create plot and regression model for PLACE OF BIRTH
 ## method. 
 PB.plot <- ggplot(PB.table, aes(x=survey.date, y=ratio)) + geom_point(size = 2) + 
         labs(title = "Ratio of weighed proportions of rural/ urban population enrolled as first year undergraduate students", 
-             subtitle = "Rural and urban identification by place of birth. Population 17-39 y/o.",
+             subtitle = "Rural and urban identification by place of birth. Population 17-39 y/o. Period Dec 2013 to Sep 2019.",
              x ="survey date", y ="proportion ratio")                                        
 
-mod1 <- lm(ratio ~ survey.date, data= PB.table) 
-summary(mod1)
-PB.plot2 <- PB.plot + geom_smooth(method = "lm", 
-                                              col = "blue")
+print(PB.plot)
 
 ## GOAL: Create plot and regression model for CURRENT ADDRESS
 ## and REASON FOR MIGRATION method. 
 PA.plot <- ggplot(PA.table, aes(x=survey.date, y=ratio)) + geom_point(size = 2) + 
         labs(title = "Ratio of weighed proportions of rural/ urban population enrolled as first year undergraduate students", 
-             subtitle = "Rural and urban identification by current address and reasons for migration. Population 17-39 y/o.",
+             subtitle = "Rural and urban identification by current address and reasons for migration. Population 17-39 y/o. Period Dec 2007 to Sep 2019.",
              x ="survey date", y ="proportion ratio") 
 
-mod2 <- lm(ratio ~ survey.date, data= PA.table) 
-summary(mod2)
-PA.plot2 <- PA.plot + geom_smooth(method = "lm", 
-                                  col = "blue")
+print(PA.plot)
+
 
